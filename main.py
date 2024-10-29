a84dd336abd9de53697ebe2b274034c849589271
ReinisDelvers <141557986+ReinisDelvers@users.noreply.github.com>
2024-10-29 07:08:16 +0200

    .

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def skoleni():
+    return render_template("skoleni.html")
+
+@app.route("/skolotaji")
+def skolotaji():
+    return render_template("skolotaji.html")
+
+@app.route("/atzimes")
+def atzimes():
+    return render_template("atzimes.html")
+
+@app.route("/tabula")
+def tabula():
+    return render_template("tabula.html")
 
 
 if __name__ == '__main__':
