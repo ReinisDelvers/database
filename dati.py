7350c734b10b9c3c6b4c3a4ff113a2490a95cb7e
ReinisDelvers <reinis.delvers@gmail.com>
2024-10-31 17:07:29 +0200

    ,

--- dati.py
+++ dati.py
@@ -2,11 +2,25 @@ import sqlite3
 
 conn = sqlite3.connect("dati.db", check_same_thread=False)
 
+def atzimju_tabulas_izveide():
+    cur = conn.cursor()
+    cur.execute(
+        """
+        CREATE TABLE atzime(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        skolens INTEGER NOT NULL,
+        subject INTEGER NOT NULL,
+        grade INTEGER NOT NULL
+        )
+        """
+    )
+    conn.commit()
+
 def tabulas_izveide():
     cur = conn.cursor()
     cur.execute(
         """
-        CREATE TABLE skoleni(
+        CREATE TABLE skolotaju(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         vards TEXT NOT NULL,
         uzvards TEXT NOT NULL
@@ -15,7 +29,7 @@ def tabulas_izveide():
     )
     conn.commit()
 
-# tabulas_izveide()\
+# tabulas_izveide()
 
 def pievienot_skolenu(vards, uzvards):
     cur = conn.cursor()
@@ -25,3 +39,54 @@ def pievienot_skolenu(vards, uzvards):
         """
     )
     conn.commit()
+
+def pievienot_skolotaju(vards, uzvards):
+    cur = conn.cursor()
+    cur.execute(
+        f"""
+        INSERT INTO skolotaju(vards, uzvards) VALUES("{vards}","{uzvards}")
+        """
+    )
+    conn.commit()
+
+def pievienot_atzime(vards, subject, grade):
+    cur = conn.cursor()
+    cur.execute(
+        f"""
+        INSERT INTO atzime(vards, subject, grade) VALUES("{vards}","{subject}","{grade}")
+        """
+    )
+    conn.commit()
+
+def iegut_skolenus():
+    cur = conn.cursor()
+    cur.execute(
+        f"""
+        SELECT vards, uzvards FROM skoleni
+        """
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
+
+def iegut_skolotaju():
+    cur = conn.cursor()
+    cur.execute(
+        f"""
+        SELECT vards, uzvards FROM skolotaju
+        """
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
+
+def iegut_atzime():
+    cur = conn.cursor()
+    cur.execute(
+        f"""
+        SELECT vards, subject, grade FROM atzime
+        """
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from dati import pievienot_skolenu
+from dati import pievienot_skolenu, pievienot_skolotaju, pievienot_atzime, iegut_skolenus, iegut_skolotaju, iegut_atzime
 import sqlite3
 
 conn = sqlite3.connect("dati.db")
@@ -8,25 +8,50 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def skoleni():
+    skolenu_db = iegut_skolenus()
+    print(skolenu_db)
     if request.method == "POST":
         vards = request.form["name"]
         uzvards = request.form["lastname"]
-        pievienot_skolenu(vards, uzvards)
+        if vards and uzvards:
+          pievienot_skolenu(vards, uzvards)
         dati = vards+" "+uzvards
-        return render_template("skoleni.html", aizsutitais = dati)
+        return render_template("skoleni.html", aizsutitais = dati, skoleni = skolenu_db)
     return render_template("skoleni.html")
 
 @app.route("/skolotaji", methods=["POST", "GET"])
-def skolotaji():
+def skolotaju():
+    skolotaju_db = iegut_skolotaju()
+    print(skolotaju_db)
+    if request.method == "POST":
+        skvards = request.form["skvards"]
+        skuzvards = request.form["skuzvards"]
+        if skvards and skuzvards:
+            pievienot_skolotaju(skvards, skuzvards)
+        dati = skvards+" "+skuzvards
+        return render_template("skolotaji.html", aizsutitais = dati, skolotaju = skolotaju_db)
     return render_template("skolotaji.html")
 
 @app.route("/atzimes", methods=["POST", "GET"])
 def atzimes():
+    atzime_db = iegut_atzime()
+    print(atzime_db)
+    if request.method == "POST":
+        atname = request.form["atvards"]
+        atsubject = request.form["atsubject"]
+        grade = request.form["grade"]
+        pievienot_atzime(atname, atsubject, grade)
+        dati = atname+" "+atsubject+" "+grade
+        return render_template("atzimes.html", aizsutitais = dati, atzime = atzime_db)
     return render_template("atzimes.html")
 
 @app.route("/tabula", methods=["POST", "GET"])
 def tabula():
-    return render_template("tabula.html")
+    skolenu_db = iegut_skolenus()
+    skolotaju_db = iegut_skolotaju()
+    atzime_db = iegut_atzime()
+    return render_template("tabula.html", atzime = atzime_db, skolotaju = skolotaju_db, skoleni = skolenu_db)
+
 
 
 if __name__ == '__main__':
